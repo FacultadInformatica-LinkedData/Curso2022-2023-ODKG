f1ac090e8baa3bb3df425cd4b436919effd3b3e8
guillaume <guillaume.champtoussel@ensea.fr>
2022-09-30 14:43:32 +0200

    fixing task8 (might be expensive)

--- Assignment4/GuillaumeChamp-220829/task08.py
+++ Assignment4/GuillaumeChamp-220829/task08.py
@@ -20,8 +20,13 @@ g2.parse(github_storage + "/rdf/data02.rdf", format="xml")
 """Tarea: lista todos los elementos de la clase Person en el primer grafo (data01.rdf) y completa los campos (given name, family name y email) que puedan faltar con los datos del segundo grafo (data02.rdf). Puedes usar consultas SPARQL o iterar el grafo, o ambas cosas."""
 
 data = Namespace("http://data.org#")
+vcard = Namespace("http://www.w3.org/2001/vcard-rdf/3.0#")
 for s, p, o in g1.triples((None, RDF.type, data.Person)):
-    for ss, pp, oo in g2.triples((s, None, None)):
+    for ss, pp, oo in g2.triples((s, vcard.Given, None)):
+        g1.add((s, pp, oo))
+    for ss, pp, oo in g2.triples((s, vcard.Family, None)):
+        g1.add((s, pp, oo))
+    for ss, pp, oo in g2.triples((s, vcard.Email, None)):
         g1.add((s, pp, oo))
 # print g1
 print("graph 1")

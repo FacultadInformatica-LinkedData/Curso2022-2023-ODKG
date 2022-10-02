af58ac3af61b6fae8a63385980da737108a150fd
Yichun Sun <113631251+yichun77@users.noreply.github.com>
2022-10-02 17:21:48 +0200

    Updated Task06.py

--- Assignment4/yichun77-22A350/Task06.py
+++ Assignment4/yichun77-22A350/Task06.py
@@ -9,7 +9,7 @@ Original file is located at
 **Task 06: Modifying RDF(s)**
 """
 
-!pip install rdflib 
+#!pip install rdflib 
 github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2021-2022/master/Assignment4/course_materials"
 
 """Leemos el fichero RDF de la forma que lo hemos venido haciendo"""

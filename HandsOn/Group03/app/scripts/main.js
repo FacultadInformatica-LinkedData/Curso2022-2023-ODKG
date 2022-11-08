4720f2bdd0179378d695101003c8ffaf2532e6a7
LuisOB33 <luis.ortiz.benito@alumnos.upm.es>
2022-11-08 17:37:08 +0100

    implement map generation dynamically by api example

--- HandsOn/Group03/app/scripts/main.js
+++ HandsOn/Group03/app/scripts/main.js
@@ -18,7 +18,28 @@ $( document ).ready(function() {
         let schoolName = $('#schoolNameSearchBox').val(); //event.currentTarget[0].value;
         if(schoolName == null || schoolName == "" ) return null; 
         alert(schoolName);
+        
+        // Example on how to add a map dinamically
+
+        // Where you want to render the map.
+        var element = $('.schoolMap');
+        element = element[0]
+        // Height has to be set. You can do this in CSS too.
+        element.style = 'height:300px;';
+        // Create Leaflet map on map element.
+        var map = L.map(element);
+        // Add OSM tile layer to the Leaflet map.
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        // Target's GPS coordinates.
+        var target = L.latLng('47.50737', '19.04611');
+        // Set map's center to target with zoom 14.
+        map.setView(target, 14);
+        // Place a marker on the same location.
+        L.marker(target).addTo(map);
         $("#searchResults").show();
+
     });
     $('.schoolItem').on('click', flipItem);
 });

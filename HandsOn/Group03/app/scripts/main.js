9bbb88ccbb7eeb160954ec55d08ebd897285c81f
agonzalez <51033256+Ofeucor@users.noreply.github.com>
2022-11-12 18:20:19 +0100

    Update Htmls + 1st sparql

--- HandsOn/Group03/app/scripts/main.js
+++ HandsOn/Group03/app/scripts/main.js
@@ -10,18 +10,33 @@ jQuery.fn.shake = function(interval,distance,times){
   return jTarget.animate({ left: 0},interval);
 }
 
+$("#schoolNameSearchBox").val("")
+
+function search(schoolName = ""){
+    let searchResults = getSpark(schoolName);
+
+    console.log(searchResults);
+
+   	generateCards(searchResults);
+
+    getMap();
+    $("#searchResults").show();
+}
+
+function generateCards(results){for(let cardResult of results){getCard(cardResult);}}
+
+function getcard(school){
+	console.log('d')
+	$("#searchResults").html('hola')
+}
+
 $( document ).ready(function() {
     var savedSchools = [];
     $('#SchoolSearchForm').on('submit', (event) => {
         event.preventDefault();
         // handle the form data
         let schoolName = $('#schoolNameSearchBox').val(); //event.currentTarget[0].value;
-        if(schoolName == null || schoolName == "" ) return null; 
-        alert(schoolName);
-        
-        // Example on how to add a map dinamically
-        getMap();
-        $("#searchResults").show();
+        search(schoolName)
 
     });
     $('.schoolInfo').on('click', flipItem);
@@ -53,3 +68,62 @@ function getMap(){
     // Place a marker on the same location.
     L.marker(target).addTo(map);
 }
+
+function getSpark(nameSearch){
+
+	var p;
+
+    $.ajax({
+    //url: "http://localhost:8080/sparql?query=PREFIX%20ont%3A%20%3Chttp%3A%2F%2Fsmartcity.linkeddata.es%2Fschoolfinder%2Fontology%2F%3E%0A%0ASELECT%20%3Fs%20%3Fname%20WHERE%20%7B%0A%20%20%3Fs%20a%20ont%3ASchool.%0A%20%20%3Fs%20ont%3Aname%20%3Fname%0A%7D%20%0ALIMIT%2010",
+
+
+    url: "http://localhost:8080/sparql?query=PREFIX%20ont%3A%20%3Chttp%3A%2F%2Fsmartcity.linkeddata.es%2Fschoolfinder%2Fontology%2F%3E%0A%0ASELECT%20%3Fs%20%3Fn%20WHERE%20%7B%0A%20%20%3Fs%20a%20ont%3ASchool%20.%0A%20%20%3Fs%20ont%3Aname%20%3Fn.%0A%20%20%0A%20%20filter%20contains%28%3Fn%2C%22"+nameSearch+"%22%29%0A%0A%7D%20%0A",
+    headers: {
+              'accept': 'application/json',
+              'Access-Control-Allow-Origin':'*'
+            },
+    type: "GET", /* or type:"GET" or type:"PUT" */
+    dataType: "json",
+    data: {
+    },
+    ContentType : false,
+    success: function (result) {
+    	//console.log(result);
+    	p = preprocess(result)
+    	console.log(p)
+
+    	
+        
+    },
+    error: function () {
+        console.log("error");
+    }
+});
+
+    return p
+}
+
+function preprocess(json){
+	var columnName = json['head']['vars']
+
+	var values = json['results']['bindings']
+
+
+	let results = []
+
+
+	for (var r of values){
+
+		var dictionary = {}
+
+		for (var c of columnName){
+			dictionary[c] = r[c].value
+		}
+		results.push(dictionary)
+	}
+
+
+	return results
+}
+
+

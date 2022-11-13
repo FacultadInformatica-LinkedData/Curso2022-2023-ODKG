dd91ec49a006404f0dd646ff035112549742013c
alexiahc <103626666+alexiahc@users.noreply.github.com>
2022-11-13 12:53:09 +0100

    Update app2.py

--- HandsOn/Group02/app2.py
+++ HandsOn/Group02/app2.py
@@ -10,6 +10,10 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import pandas as pd
 from SPARQLWrapper import SPARQLWrapper, JSON
+import tkintermapview 
+import folium
+import customtkinter
+
 
 df = pd.read_csv("AccidentesBicicletas-2019-2022-updated-with-links.csv")
 
@@ -42,33 +46,51 @@ class DemoWidget(tk.Frame):
         self.pack()
 
     def _create_gui(self):
-        label = tk.Label(self, text="Un choix")
+        self.grid_columnconfigure(0, weight=0)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self, width=150, corner_radius=0, fg_color=None)
+        self.frame_left.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
+
+        self.frame_right = customtkinter.CTkFrame(master=self, corner_radius=0)
+        self.frame_right.grid(row=0, column=1, rowspan=1, pady=0, padx=0, sticky="nsew")
+        
+        self.frame_left.grid_rowconfigure(2, weight=1)
+        
+        label = tk.Label(master=self.frame_left, text="Un choix")
         label.grid(column=0, row=1)
 
-        combo = ttk.Combobox(self, text="choix", values=DISTRICTS, 
+        combo = ttk.Combobox(master=self.frame_left, text="choix", values=DISTRICTS, 
                              textvariable=self.champs['choix'])
         combo.grid(column=1, row=1, columnspan=2)
         self.champs['choix'].set(combo.get())
 
-        label = tk.Label(self, text="Un choix")
+        label = tk.Label(master=self.frame_left, text="Un choix")
         label.grid(column=0, row=2)
 
-        combo = ttk.Combobox(self, text="choix2", values=TYPE_ACCIDENT, 
+        combo = ttk.Combobox(master=self.frame_left, text="choix2", values=TYPE_ACCIDENT, 
                              textvariable=self.champs['choix2'])
         combo.grid(column=1, row=2, columnspan=2)
 
-        label = tk.Label(self, text="elec bycicle ?")
+        label = tk.Label(master=self.frame_left, text="elec ?")
         label.grid(column=0, row=3)
 
         for i, rb_label in enumerate(IS_ELEC):
-            rb = ttk.Radiobutton(self, text=rb_label, value=i)
+            rb = ttk.Radiobutton(master=self.frame_left, text=rb_label, value=i)
             rb.grid(column=i+1, row=3)
 
-        checkButton = ttk.Checkbutton(self, text="Accepter les conditions")
+        checkButton = ttk.Checkbutton(master=self.frame_left, text="Accepter les conditions")
         checkButton.grid(column=0, row=4, columnspan=3)
 
-        button = tk.Button(self, text="Valider", command=self.valider)
+        button = tk.Button(master=self.frame_left, text="Valider", command=self.valider)
         button.grid(column=1, row=5)
+        
+        self.frame_right.grid_rowconfigure(1, weight=1)
+        self.frame_right.grid_rowconfigure(0, weight=0)
+        self.frame_right.grid_columnconfigure(0, weight=1)
+        self.frame_right.grid_columnconfigure(1, weight=0)
+        self.frame_right.grid_columnconfigure(2, weight=1)
 
 
     def valider(self):
@@ -96,8 +118,6 @@ class DemoWidget(tk.Frame):
 
         results = sparql.query().convert()
         
-        canvas = tk.Canvas(self, width=600, height=300)
-        canvas.grid(row=6)
         i = 0
 
         for result in results["results"]["bindings"]:
@@ -109,11 +129,18 @@ class DemoWidget(tk.Frame):
             out = "{}\tLfr\t{}".format(qid, label)
             print(out)
             
-            instructions = tk.Label(self, text=out, font='Raleway')
+            instructions = tk.Label(self.frame_left, text=out, font='Raleway')
             instructions.grid(row=i)
             i+=1
+        
+        map_widget = tkintermapview.TkinterMapView(self.frame_right, width=800, height=600, corner_radius=0)
+        map_widget.grid(row=1, rowspan=1, column=0, columnspan=3, sticky="nswe", padx=(0, 0), pady=(0, 0))
+        map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        map_widget.set_position(40.4167, -3.70325) 
+        map_widget.set_zoom(15)
+        marker_1 = map_widget.set_address("calle gran via, 36, madrid, spain", marker=True)
+        marker_1.set_text("place sol")
             
-
 
 app = tk.Tk()
 app.title("Demo Widgets")
@@ -121,13 +148,9 @@ DemoWidget(app)
 app.mainloop()
 
 #%%
-
-import folium
 c = folium.Map(location=[40.4167, -3.70325], zoom_start=15) 
 folium.Marker([40.4167, -3.70325],popup="La Romantica").add_to(c)
 c.save('maCarte1.html')
-
-
 
 
 
